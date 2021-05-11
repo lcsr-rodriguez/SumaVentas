0458e28aac215504b13604ac5345213286e87b2d
lcsr-rodriguez <lcsr.rodriguez@gmail.com>
2021-05-11 15:48:35 -0500

    add js para generar archivos .txt

--- assets/js/generar_json.js
+++ assets/js/generar_json.js
@@ -1,12 +1,17 @@
+/**
+ * @author          Leiner Ceballos Rodriguez
+ * @version         npm 6.14.8
+ * @description     Genera una cantidad de datos a partir de una URI
+ */
 
+// Retorna a partir de una promise un objeto JSON
+ async function getRandomData() {
+  try {
+    const data = await axios.get("https://random-data-api.com/api/commerce/random_commerce?size=6");
+    return data;
+  } catch(err) {
+    console.log("error: ", err);
+  }
+}
 
-
-const getUsers = () => {
-    axios.get('https://reqres.in/api/users')
-    .then(response => {
-     const users = response.data.data;
-     console.log(`GET users`, users);
-   })
-    .catch(error => console.error(error));
-   };
-   getUsers();
+console.log( getRandomData() )
